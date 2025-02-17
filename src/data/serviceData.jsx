0d6b381d8cc5e0bbc32e5695cbce94a770fdbcd3
Fernanda Tollotti <fernandatollotti@gmail.com>
2025-02-17 16:20:51 -0300

    feat: add service data file

---
+++ src/data/serviceData.jsx
@@ -0,0 +1,46 @@
+export const serviceData = [
+  {
+    key: "web-development",
+    image: require("../assets/service-web-development.jpg"),
+    altText: "Um escritório moderno com um laptop preto aberto exibindo um gráfico abstrato colorido na tela, fones de ouvido vermelhos, dois dispositivos de áudio, duas canecas e plantas em cima de uma mesa laranja.",
+    title: "Criação de Websites",
+    description: (
+      <p>
+        Criação de sites responsivos usando <span className="highlight">HTML, CSS, JavaScript</span> e frameworks modernos como <span className="highlight">React</span>, com foco em performance e acessibilidade.
+      </p>
+    )
+  },
+  {
+    key: "ui-ux",
+    image: require("../assets/service-ux-ui.jpg"),
+    altText: "Uma ilustração digital de um celular com design minimalista, destacando a interface de mensagens com balões coloridos e ícones intuitivos. A paleta de cores em tons de rosa e azul cria uma atmosfera convidativa e moderna.",
+    title: "Design de UI/UX",
+    description: (
+      <p>
+        Design de interfaces e experiências de usuário intuitivas e funcionais.
+      </p>
+    )
+  },
+  {
+    key: "performance",
+    image: require("../assets/service-performance.jpg"),
+    altText: "Mulher analisando a interface de um site em um computador, com elementos de UI flutuando na tela, representando otimização de desempenho",
+    title: "Performance",
+    description: (
+      <p>
+        Melhoria no desempenho de sites, otimizando tempo de carregamento e eficiência.
+      </p>
+    )
+  },
+  {
+    key: "wordpress",
+    image: require("../assets/service-wordpress.jpg"),
+    altText: "Mulher programadora digitando em um computador, com códigos refletidos na tela e um ambiente tecnológico ao fundo.",
+    title: "Temas WordPress",
+    description: (
+      <p className="tooltip-text">
+        Desenvolvimento e personalização de temas <span className="highlight">WordPress</span> para atender necessidades específicas.
+      </p>
+    )
+  }
+]

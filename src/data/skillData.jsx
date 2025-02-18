e50ff2cf41a303065936ee9288c9c8632d372759
Fernanda Tollotti <fernandatollotti@gmail.com>
2025-02-18 20:15:13 -0300

    feat: add skills section with data and styles

---
+++ src/data/skillData.jsx
@@ -0,0 +1,86 @@
+export const skillData = [
+  {
+    skillKey: "frontend",
+    label: "Front-End", 
+    listTools: [
+      {
+        key: "html",
+        label: "HTML 5",
+        icon: require("../assets/icons/html5.webp"),
+        imageAlt: "Ícone do HTML 5"
+      },
+      {
+        key: "css",
+        label: "CSS 3",
+        icon: require("../assets/icons/css3.webp"),
+        imageAlt: "Ícone do CSS 3"
+      },
+      {
+        key: "js",
+        label: "JavaScript",
+        icon: require("../assets/icons/javascript.webp"),
+        imageAlt: "Ícone do JavaScript"
+      }
+    ]
+  },
+  {
+    skillKey: "backend",
+    label: "Back-End", 
+    listTools: [
+      {
+        key: "php",
+        label: "PHP",
+        icon: require("../assets/icons/php.webp"),
+        imageAlt: "Ícone do PHP"
+      }
+    ]
+  },
+  {
+    skillKey: "cms",
+    label: "CMS", 
+    listTools: [
+      {
+        key: "wordpress",
+        label: "WordPress",
+        icon: require("../assets/icons/wordpress.webp"),
+        imageAlt: "Ícone do Wordpress"
+      }
+    ]
+  },
+  {
+    skillKey: "library",
+    label: "Biblioteca", 
+    listTools: [
+      {
+        key: "react",
+        label: "React",
+        icon: require("../assets/icons/react.webp"),
+        imageAlt: "Ícone do React"
+      }
+    ]
+  },
+  {
+    skillKey: "plataforms",
+    label: "Ferramentas & Plataformas", 
+    listTools: [
+      {
+        key: "git",
+        label: "Git",
+        icon: require("../assets/icons/git.webp"),
+        imageAlt: "Ícone do Git"
+      },
+      {
+        key: "github",
+        label: "GitHub",
+        icon: require("../assets/icons/github.webp"),
+        imageAlt: "Ícone do Github"
+      },
+      {
+        key: "figma",
+        label: "Figma",
+        icon: require("../assets/icons/figma.webp"),
+        imageAlt: "Ícone do Figma"
+      }
+    ]
+  }
+];
---
+++ src/pages/Skills/Skills.jsx
@@ -0,0 +1,43 @@
+import SectionHeader from "../../components/SectionHeader/SectionHeader";
+import { skillData } from "../../data/skillData";
+import "./Skills.css";
+
+function Skills() {
+  return (
+    <section className="skills">
+      <SectionHeader>
+        <p className="section-header__label">&#123; Habilidades &#125;</p>
+        <h2 className="section-header__heading">Minhas <span className="highlight">ferramentas</span> de especialidade</h2>
+      </SectionHeader>
+      <div className="skills__container">
+        <div className="skills__category">
+          {skillData.map((skillCategory)  => (
+            <div className="skills__content" key={skillCategory.skillKey}>
+              <p className="skills__category-title">
+                <strong>{skillCategory.label}: </strong>
+              </p>
+              <p className="skills__list">
+                {skillCategory.listTools.map(tool => tool.label).join(", ")}
+              </p>
+            </div>
+          ))}
+        </div>
+
+        <div className="skills__tools">
+          <ul className="skills__cards">
+          {skillData.map(skillCategory => (
+              skillCategory.listTools.map(tool => (
+                <li className="skills__item" key={tool.key}>
+                  <img src={tool.icon} alt={tool.imageAlt} loading="lazy" />
+                  <span className="skills__label">{tool.label}</span>
+                </li>
+              ))
+            ))}
+          </ul>
+        </div>
+      </div>
+    </section>
+  )
+}
+
+export default Skills;

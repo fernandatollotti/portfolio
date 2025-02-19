860b539355dc7242998578138b527db78e4b1840
Fernanda Tollotti <fernandatollotti@gmail.com>
2025-02-19 18:04:56 -0300

    fix: adjust active class in menu and remove component nav button import

--- src/components/Menu/Menu.jsx
+++ src/components/Menu/Menu.jsx
@@ -1,4 +1,3 @@
-import NavButton from "../NavButton/NavButton";
 import { NavLink } from "react-router-dom";
 import { User, Briefcase, Package, SquaresFour, Shapes } from "@phosphor-icons/react";
 import "./Menu.css";
@@ -7,31 +6,31 @@ function Menu() {
   return (
     <ul className="menu">
       <li className="button-group__item">
-        <NavLink to="/" className="button-group__link" activeClassName="active">
+        <NavLink to="/" className={({ isActive }) => isActive ? "button-group__link active" : "button-group__link"}>
           <User size={24} />
         </NavLink>
         <span className="tooltip">Sobre</span>
       </li>
       <li className="button-group__item" >
-        <NavLink to="/curriculo" className="button-group__link">
+        <NavLink to="/curriculo" className={({ isActive }) => isActive ? "button-group__link active" : "button-group__link"}>
           <Briefcase size={24} />
         </NavLink>
         <span className="tooltip">Currículo</span>
       </li>
       <li className="button-group__item">
-        <NavLink to="/servicos" className="button-group__link">
+        <NavLink to="/servicos" className={({ isActive }) => isActive ? "button-group__link active" : "button-group__link"}>
           <Package size={24} />
         </NavLink>
         <span className="tooltip">Serviços</span>
       </li>
       <li className="button-group__item">
-        <NavLink to="/projetos" className="button-group__link">
+        <NavLink to="/projetos" className={({ isActive }) => isActive ? "button-group__link active" : "button-group__link"}>
           <SquaresFour size={24} />
         </NavLink>
         <span className="tooltip">Projetos</span>
       </li>
       <li className="button-group__item">
-        <NavLink to="/habilidades" className="button-group__link">
+        <NavLink to="/habilidades" className={({ isActive }) => isActive ? "button-group__link active" : "button-group__link"}>
           <Shapes size={24} />
         </NavLink>
         <span className="tooltip">Habilidades</span>

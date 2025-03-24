5788150cc3e882d6496fcd79a1fe74d9b9904d4e
Fernanda Tollotti <71778388+fernandatollotti@users.noreply.github.com>
2025-03-23 21:30:50 -0300

    feat: update title and add styling for badge

--- src/pages/About/About.jsx
+++ src/pages/About/About.jsx
@@ -10,7 +10,7 @@ function About() {
               <span>Oiê, sou a {profileData.name} 👋</span>
             <code>&lt;/span&gt;</code>
           </div>
-          <h1>Júnior <span className="highlight">&#123; {profileData.job} &#125;</span> &amp; {profileData.services[1]}</h1>
+          <h1><span className="badge">Freelancer</span> <span className="highlight">&#123; {profileData.job} &#125;</span> &amp; {profileData.services[1]}</h1>
           <code>&lt;p&gt;</code>
             <p>Especializada na criação de interfaces digitais, com foco em desenvolver soluções eficientes e visualmente impactantes. Possuo experiência nas principais tecnologias do mercado, como <span className="highlight">HTML5, CSS3, JavaScript, React e WordPress.</span>
             </p>
